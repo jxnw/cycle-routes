24a45048f9e8b2c5bb789f211efb445c0e9faae5
jxnw <58523003+jxnw@users.noreply.github.com>
2023-07-03 22:02:20 +0100

    plot clusters

--- scripts/example.py
+++ scripts/example.py
@@ -14,6 +14,15 @@ def eval_way(way, config: Config = Config()):
     return score / max_score
 
 
+def filter_ways(result, threshold):
+    ways = []
+    for way in result.ways:
+        score = eval_way(way)
+        if score > threshold:
+            ways.append(way)
+    return ways
+
+
 def ways_to_edges(ways: List[overpy.Way]):
     link_counter: Dict[str, int] = {}
     for way in ways:
@@ -27,8 +36,8 @@ def ways_to_edges(ways: List[overpy.Way]):
         nodes = way.get_nodes(resolve_missing=True)
         if len(nodes) == 2:
             edges.append([nodes[0].id, nodes[1].id])  # add way as an edge
-            all_nodes.add(nodes[0].id)
-            all_nodes.add(nodes[1].id)
+            all_nodes.add(nodes[0])
+            all_nodes.add(nodes[1])
             continue
         head = nodes[0]
         tail = nodes[len(nodes) - 1]
@@ -38,33 +47,30 @@ def ways_to_edges(ways: List[overpy.Way]):
             if link_counter[node.id] > 1:
                 # break the way on node
                 edges.append([prev.id, node.id])
-                all_nodes.add(prev.id)
-                all_nodes.add(node.id)
+                all_nodes.add(prev)
+                all_nodes.add(node)
                 prev = node
                 if i == len(nodes) - 2:
                     edges.append([node.id, tail.id])
-                    all_nodes.add(tail.id)
+                    all_nodes.add(tail)
     return edges, list(all_nodes)
 
 
 def main():
     api = overpy.Overpass()
-    result = api.query("nwr(56.3284, -2.8350, 56.3437, -2.7855); out;")  # st andrews
-    # result = api.query("nwr(56.3375, -2.8059, 56.3437, -2.7855); out;")
+    # result = api.query("nwr(56.3284, -2.8350, 56.3437, -2.7855); out;")  # st andrews
+    result = api.query("nwr(56.3375, -2.8059, 56.3437, -2.7855); out;")
 
-    ways = []
-    for way in result.ways:
-        score = eval_way(way)
-        if score > 0:
-            ways.append(way)
+    ways = filter_ways(result, -1)
 
     edges, all_nodes = ways_to_edges(ways)
-    node_mapping = {node_id: idx for idx, node_id in enumerate(all_nodes)}
+    node_mapping = {node.id: idx for idx, node in enumerate(all_nodes)}
     mapped_edges = [[node_mapping[edge[0]], node_mapping[edge[1]]] for edge in edges]
+    layout = [(float(node.lon), float(node.lat)) for node in all_nodes]
 
     g = ig.Graph(n=len(all_nodes), edges=mapped_edges)
     fig, ax = plt.subplots()
-    ig.plot(g, target=ax)
+    ig.plot(g, target=ax, layout=layout, vertex_size=0.0002, bbox=(800, 400))
     plt.show()
 
 
